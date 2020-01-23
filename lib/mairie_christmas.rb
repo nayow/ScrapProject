6ac600c518b5948df843571887d82790a2c5dbc6
nayow <nayow@hotmail.fr>
2020-01-24 00:10:02 +0100

    added comments

--- lib/mairie_christmas.rb
+++ lib/mairie_christmas.rb
@@ -16,7 +16,7 @@ def get_townhall_urls
 
     page = Nokogiri::HTML(open("https://www.annuaire-des-mairies.com/val-d-oise.html"))
 
-    urls = page.xpath('//tr/td[1]/p/a/@href')
+    urls = page.xpath('//tr/td[1]/p/a/@href') #limité à la première colonne de villes
     urls.each {|url| urls_array << "https://www.annuaire-des-mairies.com"+url.text.sub(".","")}
 
     names = page.xpath('//tr/td[1]/p/a')
@@ -25,8 +25,8 @@ def get_townhall_urls
     for i in 0..urls_array.length-1
         result << {names_array[i] => get_townhall_email(urls_array[i])}
     end
-    # puts result
+    puts result
     return result
 end
 
-# get_townhall_urls
+get_townhall_urls
